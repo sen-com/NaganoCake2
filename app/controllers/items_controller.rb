fdd709eb080a9fd22fc3395288ff87af855927e1
sen-com <nogenon@docomo.ne.jp>
2020-09-02 13:32:46 +0900

    change items show

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -6,7 +6,7 @@ class ItemsController < ApplicationController
 
   def index
     @ranking_items = Item.find(OrderDetail.group(:item_id).order('sum(quantitty) desc').limit(3).pluck(:item_id))
-    binding.pry
+    # binding.pry
     @items = Item.where(is_working: "true").search(params[:search])
   end
 
